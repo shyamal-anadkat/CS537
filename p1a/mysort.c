02469271ad02368e964ae924b49c4f8debc20c9b
zostale <shyamalanadkat@gmail.com>
2017-01-19 16:50:05 -0600

    SA 1/19

--- p1a/mysort.c
+++ p1a/mysort.c
@@ -7,7 +7,26 @@
 int main(int argc, char** argv)
 {
 	/* Your code goes here! */
+	int nlines;
+        int opt;
 
+	nlines = 0;
+
+	while((opt = getopt(argc, argv, "rn:")) != -1) {
+		switch(opt) {
+		case 'r':
+		  printf("just read from stdin");
+		  break;
+		case 'n':
+		  nlines = atoi(optarg);
+		  printf("%d lines",nlines);
+		  break;
+		default: /* '?' */
+		 printf("Usage is wrong");
+		 exit(EXIT_FAILURE);
+	 }
+        
+	}
 	/* If successful, exit with a zero status. */
 	return 0;
 }
