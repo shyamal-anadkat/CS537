1270667963cbbaf070679c81fba840b6ebab520d
zostale <shyamalanadkat@gmail.com>
2017-01-18 18:40:23 -0600

    Initial commit

---
+++ p1a/mysort.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* See `man 3 getopt()` for how to use the getopt function. */
+#include <getopt.h>
+
+int main(int argc, char** argv)
+{
+	/* Your code goes here! */
+
+	/* If successful, exit with a zero status. */
+	return 0;
+}
