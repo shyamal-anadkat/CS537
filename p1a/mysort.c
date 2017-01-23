da9447061b7d0fcae5597f642c4e36c3f0a3c8dd
zostale <shyamalanadkat@gmail.com>
2017-01-23 16:00:10 -0600

    SA 1/23 qsort prog

--- p1a/mysort.c
+++ p1a/mysort.c
@@ -5,6 +5,11 @@
 
 #define MAX_CHAR 1024
 
+static int numLines(FILE *f);
+static char** readFile(FILE *in, char** data);
+static int cmpstr(const void* a, const void* b);
+static int cmpstrRev(const void* a, const void* b);
+
 
 void
 usage(char *prog)
@@ -18,6 +23,43 @@ int cmpstr(const void* a, const void* b) {
 	    const char* aa = *(const char**)a;
 	        const char* bb = *(const char**)b;
 		    return strcmp(aa, bb);
+}
+
+ int cmpstrRev(const void* a, const void* b) {
+	               const char* aa = *(const char**)a;
+	                    const char* bb = *(const char**)b;
+	                        return -strcmp(aa, bb);
+	     }
+
+char** readFile(FILE * in, char** data) {
+	//int cntr,llen;
+	int i = 0; 
+	char line[MAX_CHAR];
+
+	while(fgets(line,MAX_CHAR,in)!=NULL) {
+		data[i] = strdup(line);
+		//data[cntr] = calloc(sizeof(char), llen++);
+		//strcpy(data[cntr], line);
+		//cntr++;
+		i++;
+	}
+      return data;
+}
+
+
+int 
+numLines(FILE *fp){
+	int lines = 0;
+        int ch; 	
+	while(!feof(fp))
+	{
+	ch = fgetc(fp);
+	if(ch == '\n') {
+	lines++;
+				    }
+	}
+      printf("lines: %d\n",lines);
+      return lines; 
 }
  
 int main(int argc, char** argv)
@@ -57,7 +99,20 @@ int main(int argc, char** argv)
 	while((opt = getopt(argc, argv, "rn:")) != -1) {
 		switch(opt) {
 		case 'r':
-		  printf("read from stdin and reverse");
+		   if(argv[optind] != NULL){
+		   FILE *fp = fopen(argv[optind],"r");
+		   if(fp == NULL) {
+			fprintf(stderr,"Error opening file.\n");
+			return -1;
+		   }
+		   int lines = numLines(fp);
+		   char **text = malloc(sizeof(char *) * lines); 
+	           text = readFile(fp, text);
+		   //printf("lines:%d", lines); 
+		   for(i = 0; i < lines; i++) {
+			 printf("%s\n", text[i]);
+			}
+	           }
 		  break;
 		case 'n':
 		  nlines = atoi(optarg);
@@ -68,7 +123,8 @@ int main(int argc, char** argv)
 		 //exit(EXIT_FAILURE);
 	 }
         }
-
+        
+	printf("%s", argv[optind]);
 	/*If successful, exit with a zero status. */
 	return 0;
 }
