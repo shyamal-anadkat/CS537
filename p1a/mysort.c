c68609afa3165980f8bf86df923f5d08339d3350
zostale <shyamalanadkat@gmail.com>
2017-01-23 13:43:33 -0600

    SA 1/23

--- p1a/mysort.c
+++ p1a/mysort.c
@@ -6,30 +6,52 @@
 #define MAX_CHAR 1024
 
 
+void
+usage(char *prog)
+{
+	fprintf(stderr,"usage: %s <option>\n", prog);
+	exit(1);
+
+}
+
+int cmpstr(const void* a, const void* b) { 
+	    const char* aa = *(const char**)a;
+	        const char* bb = *(const char**)b;
+		    return strcmp(aa, bb);
+}
+ 
 int main(int argc, char** argv)
 {
 	/* Your code goes here! */
-	int nlines;
+	int nlines,linecnt,i;
         int opt;
-	char *input = calloc(1,1), buffer[MAX_CHAR];
-	FILE *fileIN, *fileOUT;
-	nlines = 0;
+	char *input[MAX_CHAR];
+	char buffer[1024];
+	//char **input = malloc(5*sizeof(char*));
+        //FILE *fileIN, *fileOUT;
 
 	if(argc == 1) {
 		printf("just read from stdin\n");
-		while(fgets(buffer, MAX_CHAR, stdin)) 
-		{      
-		 input = realloc(input, strlen(input)+1+strlen(buffer));
-		 strcat(input, buffer); 
+		while(fgets(buffer, MAX_CHAR, stdin)!= NULL) 
+		{  
+	         //input[i] = (char*) malloc(MAX_CHAR);	
+		 //input =  realloc(input, strlen(input)+1+strlen(buffer));
+		 //input[i++] = malloc(i * sizeof(char *));
+		 //input[i] = buffer
+		 //memcpy(input[i],buffer,strlen(buffer));
+		 input[i++] = strdup(buffer);
+		 //strcat(input, buffer);
+		 linecnt++;
 		}
 
-          //qsort(words,count,size,strcmp);
-	  char *sorted = malloc(sizeof(*input));
-	  
-	  printf("\ninput:\n%s",input);
-	  printf("%d",sizeof(input));	
-	  // printf("\nsorted:\n%s",sorted);	
-		
+          
+          qsort(input,linecnt,sizeof(char*),cmpstr);
+	  //char *sorted = malloc(sizeof(*input));
+	  for(i = 0; i < linecnt; i++) {
+	  printf("%s",input[i]);
+	   }
+	  printf("%d\n",linecnt);
+	  //free(input);	  
 	}
 	
 	while((opt = getopt(argc, argv, "rn:")) != -1) {
@@ -42,8 +64,8 @@ int main(int argc, char** argv)
 		  printf("%d lines",nlines);
 		  break;
 		default: /* '?' */
-		 printf("Usage is wrong");
-		 exit(EXIT_FAILURE);
+		 usage(argv[0]);
+		 //exit(EXIT_FAILURE);
 	 }
         }
 
