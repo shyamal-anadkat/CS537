2f450aec3a6b86a8faa7cca16cbdb91c98679e3b
zostale <shyamalanadkat@gmail.com>
2017-01-24 15:24:14 -0600

    SA 1/24 almost done

--- p1a/mysort.c
+++ p1a/mysort.c
@@ -15,107 +15,135 @@ static int cmpstrRev(const void* a, const void* b);
 void
 usage(char *prog)
 {
-	fprintf(stderr,"usage: %s <option>\n", prog);
-	exit(1);
+    fprintf(stderr,"usage: %s <option>\n", prog);
+    exit(1);
 
 }
 
-int cmpstr(const void* a, const void* b) { 
-	    const char* aa = *(const char**)a;
-	        const char* bb = *(const char**)b;
-		    return strcmp(aa, bb);
+//ascending strcmp wrapper
+int cmpstr(const void* a, const void* b) {
+    const char* aa = *(const char**)a;
+    const char* bb = *(const char**)b;
+    return strcmp(aa, bb);
 }
 
- int cmpstrRev(const void* a, const void* b) {
-	               const char* aa = *(const char**)a;
-	                    const char* bb = *(const char**)b;
-	                        return -strcmp(aa, bb);
-	     }
+//descending strcmp wrapper
+int cmpstrRev(const void* a, const void* b) {
+    const char* aa = *(const char**)a;
+    const char* bb = *(const char**)b;
+    return -strcmp(aa, bb);
+}
 
+//read input from file into array  
 char** readFile(FILE * in, char** data) {
-	int i = 0; 
-	char line[MAX_CHAR];
-	while(fgets(line,MAX_CHAR,in)!=NULL) {
-		data[i] = strdup(line);
-		i++;
-	}
-      return data;
+    int i = 0;
+    char line[MAX_CHAR];
+    while(fgets(line,MAX_CHAR,in)!=NULL) {
+        data[i] = strdup(line);
+        i++;
+    }
+    return data;
 }
 
-
-int 
-numLines(FILE *fp){
-	int lines = 0;
-        int ch; 	
-	while(!feof(fp))
-	{
-	ch = fgetc(fp);
-	if(ch == '\n') {
-	lines++;
-				    }
-	}
-      printf("lines: %d\n",lines);
-      return lines; 
+//one pass file line reader 
+int
+numLines(FILE *fp) {
+    int lines = 0;
+    int ch;
+    while(!feof(fp))
+    {
+        ch = fgetc(fp);
+        if(ch == '\n') {
+            lines++;
+        }
+    }
+    //printf("lines: %d\n",lines);
+    return lines;
 }
- 
+
 int main(int argc, char** argv)
 {
-	/* Your code goes here! */
-	int nlines,linecnt,i;
-        int opt;
-	char *input[MAX_CHAR];
-	char buffer[1024];
-	//char **input = malloc(5*sizeof(char*));
-	if(argc == 1) {
-		printf("just read from stdin\n");
-		while(fgets(buffer, MAX_CHAR, stdin)!= NULL) 
-		{  
-	         //input[i] = (char*) malloc(MAX_CHAR);	
-		 //input =  realloc(input, strlen(input)+1+strlen(buffer));
-		 //input[i++] = malloc(i * sizeof(char *));
-		 //input[i] = buffer
-		 //memcpy(input[i],buffer,strlen(buffer));
-		 input[i++] = strdup(buffer);
-		 linecnt++;
-		}
-
-          
-          qsort(input,linecnt,sizeof(char*),cmpstr);
-	  //char *sorted = malloc(sizeof(*input));
-	  for(i = 0; i < linecnt; i++) {
-	  printf("[%d]: %s",i,input[i]);
-	  }	  
-	}
-	
-	while((opt = getopt(argc, argv, "rn:")) != -1) {
-		switch(opt) {
-		case 'r':
-		   if(argv[optind] != NULL){
-		   FILE *fp = fopen(argv[optind],"r");
-		   if(fp == NULL) {
-			fprintf(stderr,"Error opening file.\n");
-			return -1;
-		   }
-		   int lines = numLines(fp);
-		   //char **text = malloc(MAX_CHAR * lines); 
-		   char **text = malloc(sizeof(char *) * lines);
-		   lseek(fileno(fp), 0, SEEK_SET); 
-	           text = readFile(fp, text);
-		   qsort(text, lines, sizeof(char*),cmpstr);
-		   for(i = 0; i < lines; i++) {
-			 printf("%s\n", text[i]);
-			}
-	           }
-		  break;
-		case 'n':
-		  nlines = atoi(optarg);
-		  printf("%d lines",nlines);
-		  break;
-		  default: /* '?' */
-		 usage(argv[0]);
-		 //exit(EXIT_FAILURE);
-	 }
+ 
+    int linecnt, i;
+    int opt;
+    char *input[MAX_CHAR];
+    char buffer[1024];
+    int rflag = 0;
+    int nlines = -1; 
+    //char **input = malloc(5*sizeof(char*));
+    while((opt = getopt(argc, argv, "rn:")) != -1) {
+    switch(opt) {
+        case 'r':
+        rflag = 1; 
+            //if(argv[optind] != NULL) {
+            //}
+            printf("reverse flag set\n");
+            break;
+        case 'n':
+            nlines = atoi(optarg);
+            printf("lines set\n");
+            break;
+        default: // '?'
+            usage(argv[0]);
+            //exit(EXIT_FAILURE);
         }
-	printf("%s", argv[optind]);
-	return 0;
+    }
+
+    if(argv[optind] != NULL) {
+                FILE *fp = fopen(argv[optind],"r");
+                if(fp == NULL) {
+                    fprintf(stderr,"Error opening file.\n");
+                    return -1;
+                }
+                int lines = numLines(fp);
+                
+                char **text = malloc(sizeof(char *) * lines);
+                
+                lseek(fileno(fp), 0, SEEK_SET);
+                
+                text = readFile(fp, text);
+                
+                if(rflag == 1) {
+                qsort(text, lines, sizeof(char*),cmpstrRev);
+            } else {
+                qsort(text, lines, sizeof(char*),cmpstr);
+            }
+                
+                if(nlines != -1) {
+        for(i = 0; i < nlines; i++) {
+            printf("[%d]: %s",i,text[i]);
+        } } else {
+                for(i = 0; i < lines; i++) {
+                   printf("[%d]: %s",i,text[i]);
+                }}
+    } else {
+        printf("just read from stdin\n");
+        while(fgets(buffer, MAX_CHAR, stdin)!= NULL)
+        {
+            //input[i] = (char*) malloc(MAX_CHAR);
+            //input =  realloc(input, strlen(input)+1+strlen(buffer));
+            //input[i] = buffer
+            input[i++] = strdup(buffer);
+            linecnt++;
+        }
+        if(rflag==1) {
+        qsort(input,linecnt,sizeof(char*),cmpstrRev);
+    }else {
+        qsort(input,linecnt,sizeof(char*),cmpstr);
+    }
+        if(nlines != -1) {
+        for(i = 0; i < nlines; i++) {
+            printf("[%d]: %s",i,input[i]);
+        } }
+        else {
+            for(i = 0; i < linecnt; i++) {
+            printf("[%d]: %s",i,input[i]);
+        }
+
+        }
+    }
+    //}
+    printf("optind: %s\n", argv[optind]);
+    printf("optarg: %s\n", optarg);
+    return 0;
 }
