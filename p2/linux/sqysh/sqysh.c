eb3d349e9109744855c226fbf2a4841cae7adecf
zostale <shyamalanadkat@gmail.com>
2017-02-01 20:53:30 -0600

    SA 2/1 shell prog

--- p2/linux/sqysh/sqysh.c
+++ p2/linux/sqysh/sqysh.c
@@ -2,9 +2,46 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define MAX_ARGS 256
+
+const char PROMPT[] = "sqysh$ ";
+const char ERROR[] = "error occured.";
+const char FILE_ERROR[] = "file cannot be read.";
+int batch_flag = 0; 
+FILE *stream = NULL;
 
 int main(int argc, char** argv)
 {
-	/* Your code here! */
+
+
+	if(isatty (fileno(stdin)) && argc==1) {
+		//stream = stdin; 
+		//write (STDOUT_FILENO, PROMPT, strlen(PROMPT));
+	} else if(argc==2) {
+		stream = fopen(argv[1], "r");
+		batch_flag = 1; 
+		if(stream==NULL) {
+			write (STDERR_FILENO, FILE_ERROR, strlen(FILE_ERROR));
+			exit(EXIT_FAILURE);
+		}
+		dup2(fileno(stream), STDIN_FILENO); //re-direct to stdin
+
+	} else {
+		write (STDERR_FILENO, ERROR, strlen(ERROR));
+		exit(EXIT_FAILURE);
+	}
+
+	while(1) {
+		if(!batch_flag) {
+			write (STDOUT_FILENO, PROMPT, strlen(PROMPT));
+		}
+		 char buffer[MAX_ARGS];
+		 if (fgets(buffer, MAX_ARGS, stdin)!= NULL) {
+		 		printf("%s",buffer);
+		 }
+	}
 	return 0;
 }
